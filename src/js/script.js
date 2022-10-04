d13cde752236b489e7c936154c791790430f0174
Dmitry <s11me@yandex.ru>
2022-10-04 15:24:21 +0700

    btn scroll

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,12 @@ humburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const elem = document.querySelector('.pageup');
+window.onscroll = () => {
+    if (window.scrollY > 700){
+        elem.classList.remove('show');
+    } else if (window.scrollY < 700){
+        elem.classList.add('show');
+    }
+};
